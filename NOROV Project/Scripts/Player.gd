eb976db669cc5f35ecd24d962f7e2709c1f9a267
oleh-26-01 <yarosholeg04@gmail.com>
2022-04-10 18:25:26 +0300

    Update project parameters and initial location, new mechanics and object, added camera
    1. The screen extension of the game has been changed from 1920x1080 to 640x360. All graphic elements of the project have been adjusted according to the change of the screen extension.
    2. Added jerk mechanics with simple change of time and distance parameters, activation with [shift] button.
    3. Updated initial location. Added mini-forest, and a room with the enemy.
    4. Added smooth camera tracking of the player.
    5. Created a new object bat in the role of the enemy, added the initial animation.

--- NOROV Project/Scripts/Player.gd
+++ NOROV Project/Scripts/Player.gd
@@ -1,24 +1,51 @@
 extends KinematicBody2D
 
-const MAX_SPEED = 500
-const ACCELERATION = 2000
-const FRICTION = 4000
+export var MAX_SPEED = 320
+export var TO_MAX_TIME  = 0.2
+export var TO_STOP_TIME = 0.2
+
+var is_action_jerk = false
+export var JERK_INTERVAL = {"const_time": 2, "after_prev": 0}
+export var JERK_TIME : float = 0.1
+export var JERK_LENGTH : float = 200
 
 var velocity = Vector2.ZERO
 var input_vector = Vector2.ZERO
 
 onready var player_sprite = $Sprite
 onready var animationTree = $AnimationTree
+onready var jerk_time = $jerk_time
 
 func _physics_process(delta):
-	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	input_vector = input_vector.normalized()
+	JERK_INTERVAL["after_prev"] += delta
 	
-	if input_vector != Vector2.ZERO:
-		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
-		animationTree.set("parameters/Idle/blend_position", input_vector)
+	# show time after previous jerk and time of jerk interval
+	jerk_time.text = str(stepify(JERK_INTERVAL["after_prev"], 0.1)) + "/" + str(JERK_INTERVAL["const_time"])
+	
+	if is_action_jerk:
+		# turn off jerk action and set velocity after it
+		if JERK_INTERVAL["after_prev"] >= JERK_TIME:
+			velocity = velocity.move_toward(input_vector * MAX_SPEED, MAX_SPEED / 0.1 * delta)
+			if velocity.length() <= MAX_SPEED:
+				is_action_jerk = false
+		else:
+			velocity = input_vector * JERK_LENGTH / JERK_TIME
 	else:
-		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+		input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+		input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+		input_vector = input_vector.normalized()
+		
+		# increase / decrease player speed and set animation
+		if input_vector != Vector2.ZERO:
+			velocity = velocity.move_toward(input_vector * MAX_SPEED, MAX_SPEED / TO_MAX_TIME * delta)
+			animationTree.set("parameters/Idle/blend_position", input_vector)
+		else:
+			velocity = velocity.move_toward(Vector2.ZERO, MAX_SPEED / TO_STOP_TIME * delta)
 		
 	velocity = move_and_slide(velocity)
+
+func _input(event):
+	if event.is_action_pressed("player_jerk"):
+		if JERK_INTERVAL["after_prev"] > JERK_INTERVAL["const_time"]:
+			is_action_jerk = true
+			JERK_INTERVAL["after_prev"] = 0
--- NOROV Project/Scripts/testing_place-GUI.gd
+++ NOROV Project/Scripts/testing_place-GUI.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends CanvasLayer
 
 
 func _on_main_menubutton_pressed():
